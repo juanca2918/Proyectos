71a99717bcd855a9aa3f368ecfe9c515104f223f
Juan Carlos <42156547+juanca2918@users.noreply.github.com>
2019-06-06 20:03:57 -0500

    Actualizacion realizada en Junio 6/2019
    Realizada por Juan Carlos Arias Botero

--- Modelo-Negocio_JulianAlzate_Materialize/js/codigo.js
+++ Modelo-Negocio_JulianAlzate_Materialize/js/codigo.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
   M.AutoInit();
     $('.parallax').parallax();
-});
+    $('.sidenav').sidenav();
+    $('input.autocomplete').autocomplete({
+      data: {
+        "Apple": null,
+        "Microsoft": null,
+        "Google": 'https://placehold.it/250x250'
+      },
+    });
+  });
